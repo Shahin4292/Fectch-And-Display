8a4d8f6884e91a931fd1b0dc6fb9dc7618662783
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-10-23 12:50:19 +0600

    add model done

---
+++ lib/services/api service.dart
@@ -0,0 +1,15 @@
+import 'package:fetch/app/model/makeup_model.dart';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  static var client = http.Client();
+
+  Future<List<ProductModel>?> fetchProduct() async {
+    var response = await client.get(Uri.parse(
+        'https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline'));
+    if (response.statusCode == 200) {
+      var jsonString = response.body;
+      return productModelFromJson(jsonString);
+    }
+  }
+}
