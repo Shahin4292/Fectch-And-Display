0dd9d072aa4b77d6aaa4a009bf6f96379a06f794
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-10-23 10:59:47 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'app/view/home_view.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,11 +13,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: HomeView(),
     );
   }
 }
